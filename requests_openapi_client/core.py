2791adb743dae3a352705679e052c949be4d78e8
Andrew Pendleton <andrew.pendleton@sofarocean.com>
2021-10-26 03:10:41 -0400

    Initial refactor: realize all the methods

--- requests_openapi_client/core.py
+++ requests_openapi_client/core.py
@@ -3,9 +3,10 @@ import pprint
 import typing
 
 import requests
-import yaml
+# import yaml
 
 from .requestor import Requestor
+from .util import camel_to_snake
 
 log = logging.getLogger(__name__)
 
@@ -32,9 +33,10 @@ class OpenAPIKeyWord:
     SCHEMA = "schema"
     TYPE = "type"
     STRING = "string"
+    TAGS = "tags"
 
 
-class Server(object):
+class Server:
     _url: str
     description: str
     variables: typing.Dict[str, typing.Any]
@@ -63,13 +65,10 @@ class Operation(object):
 
     _call: typing.Optional[typing.Callable] = None
 
-    def __init__(self, path, method, spec, requestor=None, req_opts={}, server=None):
+    def __init__(self, path, method, spec,):
         self._path = path
         self._method = method
         self._spec = spec
-        self._requestor = requestor
-        self._req_opts = req_opts
-        self._server = server
 
     @property
     def spec(self):
@@ -87,11 +86,15 @@ class Operation(object):
     def path(self):
         return self._path
 
-    def url(self, **kwargs):
-        return self._server.url + self._path.format(**kwargs)
+    @property
+    def parameters(self):
+        return [spec[OpenAPIKeyWord.NAME] for spec in self._spec.get(OpenAPIKeyWord.PARAMETERS, [])]
+
+    def url(self, server, **kwargs):
+        return server.url + self._path.format(**kwargs)
 
     def _gen_call(self):
-        def f(**kwargs):
+        def f(client, **kwargs):
             # collect api params
             path_params = {}
             params = {}
@@ -125,24 +128,50 @@ class Operation(object):
             kwargs.setdefault("params", {}).update(params)
             kwargs.setdefault("headers", {}).update(headers)
             kwargs.setdefault("cookies", {}).update(cookies)
-            for k, v in self._req_opts.items():
+            for k, v in client._req_opts.items():
                 kwargs.setdefault(k, v)
-            return self._requestor.request(
-                self._method, self.url(**path_params), **kwargs
+            return client._requestor.request(
+                self._method, self.url(client._server, **path_params), **kwargs
             )
 
         return f
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, client, *args, **kwargs):
         if not self._call:
             self._call = self._gen_call()
-        return self._call(*args, **kwargs)
+        return self._call(client, *args, **kwargs)
 
     def help(self):
         return pprint.pprint(self.spec, indent=2)
 
     def __repr__(self):
         return f"<{type(self).__name__}: [{self._method}] {self._path}>"
+
+    def add_client_method(self, target):
+        op_name = camel_to_snake(self.operation_id)
+        op_locals = {"op": self}
+
+        required_params = []
+        optional_params = []
+        for spec in self._spec.get(OpenAPIKeyWord.PARAMETERS, []):
+            _in = spec[OpenAPIKeyWord.IN]
+            name = spec[OpenAPIKeyWord.NAME]
+            required = spec.get(OpenAPIKeyWord.REQUIRED, False)
+
+            if required or _in == OpenAPIKeyWord.PATH:
+                required_params.append(name)
+            else:
+                optional_params.append(name)
+
+        signature_params = ", ".join(
+            required_params +\
+            [f"{param}=None" for param in optional_params]
+        )
+        passthrough_params = ", ".join(
+            [f"{param}={param}" for param in required_params + optional_params]
+        )
+        exec(f"def {op_name}(self, {signature_params}):\n    return op(self, {passthrough_params})", op_locals)
+        setattr(target, op_name, op_locals[op_name])
 
 
 def load_spec_from_url(url):
@@ -159,7 +188,7 @@ def load_spec_from_file(file_path):
     return yaml.load(spec_str, Loader=yaml.Loader)
 
 
-class Client(object):
+class BaseClient:
     _requestor: Requestor
     _req_opts: typing.Dict[str, typing.Any]
     _server: Server
@@ -169,9 +198,13 @@ class Client(object):
     def __init__(self, requestor=None, server=None, req_opts={}):
         self._requestor = requestor or requests.Session()
         self._req_opts = req_opts
-        self._server = server
-        self._operations = {}
-        self._spec = {}
+        if server:
+            self._server = server
+        elif self.servers:
+            self._server = self.servers[0]
+
+        for sub_api in self._sub_apis:
+            setattr(self, sub_api, SubApiProxy(getattr(self, sub_api), self))
 
     @property
     def operations(self):
@@ -181,7 +214,8 @@ class Client(object):
     def spec(self):
         return self._spec
 
-    def load_spec(self, spec: typing.Dict):
+    @classmethod
+    def subclass_from_spec(base_cls, spec: typing.Dict, client_name="ApiClient"):
         if not all(
             [
                 i in spec
@@ -193,14 +227,17 @@ class Client(object):
             ]
         ):
             raise ValueError("Invaliad openapi document")
-        self._spec = spec.copy()
+
+        cls = type(client_name, (base_cls,), {})
+
+        cls._spec = spec.copy()
         _spec = spec.copy()
 
         servers = _spec.pop(OpenAPIKeyWord.SERVERS, [])
         for key in _spec:
             rkey = key.replace("-", "_")
-            self.__setattr__(rkey, _spec[key])
-        self.servers = [
+            setattr(cls, rkey, _spec[key])
+        cls.servers = [
             Server(
                 url=s.get(OpenAPIKeyWord.URL),
                 description=s.get(OpenAPIKeyWord.DESCRIPTION),
@@ -208,14 +245,15 @@ class Client(object):
             )
             for s in servers
         ]
-        if not self._server and self.servers:
-            self._server = self.servers[0]
 
-        self._collect_operations()
+        cls._collect_operations()
+        return cls
 
-    def _collect_operations(self):
-        self._operations = {}
-        for path, path_spec in self.paths.items():
+    @classmethod
+    def _collect_operations(cls):
+        cls._operations = {}
+        cls._sub_apis = []
+        for path, path_spec in cls.paths.items():
             for method, op_spec in path_spec.items():
                 operation_id = op_spec.get(OpenAPIKeyWord.OPERATION_ID)
                 if not operation_id:
@@ -228,12 +266,9 @@ class Client(object):
                     path,
                     method,
                     op_spec,
-                    requestor=self._requestor,
-                    req_opts=self._req_opts,
-                    server=self._server,
                 )
-                if operation_id not in self._operations:
-                    self._operations[operation_id] = op
+                if operation_id not in cls._operations:
+                    cls._operations[operation_id] = op
                 else:
                     log.warning(
                         f"multiple '{operation_id}' found , operation ID should be unique"
@@ -243,15 +278,29 @@ class Client(object):
                         self._operations[operation_id] = [v]
                     self._operations[operation_id].append(op)
 
-    def load_spec_from_url(self, url):
-        spec = load_spec_from_url(url)
-        self.load_spec(spec)
-        return
+                tags = op_spec.get(OpenAPIKeyWord.TAGS, None)
+                if tags:
+                    target_name = camel_to_snake(tags[0])
+                    if not hasattr(cls, target_name):
+                        setattr(cls, target_name, SubApi())
+                        cls._sub_apis.append(target_name)
+                    target = getattr(cls, target_name)
+                else:
+                    target = cls
 
-    def load_spec_from_file(self, file_path):
+                op.add_client_method(target)
+
+
+    @classmethod
+    def subclass_from_spec_url(cls, url):
+        spec = load_spec_from_url(url)
+        return cls.subclass_from_spec(spec)
+
+    @classmethod
+    def subclass_from_spec_file(cls, file_path):
         spec = load_spec_from_file(file_path)
-        self.load_spec(spec)
-        return
+        return cls.subclass_from_spec(spec)
+
 
     @property
     def requestor(self):
@@ -269,9 +318,16 @@ class Client(object):
         self._server = s
         self._collect_operations()
 
-    def __getattr__(self, op_name):
-        if op_name in self._operations:
-            return self._operations[op_name]
-        raise AttributeError(
-            f"'{type(self).__name__}' object has no attribute '{op_name}'"
-        )
+class SubApi:
+    pass
+
+class SubApiProxy:
+    def __init__(self, sub_api, client):
+        self._sub_api = sub_api
+        self._client = client
+
+    def __getattr__(self, a):
+        # raw function
+        func = getattr(self._sub_api, a)
+        # bind to client
+        return func.__get__(self._client)
