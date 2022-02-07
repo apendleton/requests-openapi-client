a371e36e32c838da7f05c87297cad0806a29e9ee
Andrew Pendleton <andrew.pendleton@sofarocean.com>
2022-02-07 12:32:21 -0500

    improve serialization of non-nullable, non-required fields

--- requests_openapi_client/schemas.py
+++ requests_openapi_client/schemas.py
@@ -18,12 +18,19 @@ TYPE_EQUIVALENTS = {
 class BaseDto:
     name_map: typing.Dict = {}
     reverse_name_map: typing.Dict = {}
+    required_fields: typing.Set = set()
+    nullable_fields: typing.Set = set()
 
-    def serialize(self):
+    def serialize(self, use_python_names=False):
         out = {}
         for field_name, field in self.__dataclass_fields__.items():
-            raw_name = self.name_map[field_name]
-            out[raw_name] = serialize_as(getattr(self, field_name), field.type)
+            value = getattr(self, field_name)
+            if value is None and field_name not in self.required_fields and field_name not in self.nullable_fields:
+                # this field isn't nullable and isn't required, so interpret it being None as "don't set"
+                # instead of "set to null"
+                continue
+            raw_name = field_name if use_python_names else self.name_map[field_name]
+            out[raw_name] = serialize_as(value, field.type, use_python_names=use_python_names)
         return out
 
     @classmethod
@@ -54,15 +61,15 @@ def deserialize_as(data, data_type):
         return dateutil.parser.parse(data)
     return data
 
-def serialize_as(data, data_type):
+def serialize_as(data, data_type, use_python_names=False):
     if data is None:
         return None
     elif type(data_type) is type and issubclass(data_type, BaseDto):
-        return data.serialize()
+        return data.serialize(use_python_names=use_python_names)
     elif typing.get_origin(data_type) is list and type(data) is list and typing.get_args(data_type):
         # TODO: what if there are multiple args?
         item_type = typing.get_args(data_type)[0]
-        return [serialize_as(item, item_type) for item in data]
+        return [serialize_as(item, item_type, use_python_names=use_python_names) for item in data]
     elif data_type is datetime.datetime and type(data) is datetime.datetime:
         return data.isoformat()
     return data
@@ -115,12 +122,21 @@ def add_types_to_module(spec, module):
         name = format_schema_name(name)
         if desc.get("type", None) == "object":
             properties = []
+            required_fields = set()
+            nullable_fields = set()
             name_map = {}
             reverse_name_map = {}
+
+            source_required = set(desc.get("required", []))
             for prop, prop_schema in desc.get("properties", {}).items():
                 snake_prop = camel_to_snake(prop)
                 name_map[snake_prop] = prop
                 reverse_name_map[prop] = snake_prop
+
+                if prop in source_required:
+                    required_fields.add(snake_prop)
+                if prop_schema.get("nullable", False):
+                    nullable_fields.add(snake_prop)
                 properties.append((
                     snake_prop,
                     type_for_schema(prop_schema, spec),
@@ -130,7 +146,10 @@ def add_types_to_module(spec, module):
             model = dataclasses.make_dataclass(name, properties, bases=(BaseDto,))
             model.name_map = name_map
             model.reverse_name_map = reverse_name_map
+            model.required_fields = required_fields
+            model.nullable_fields = nullable_fields
             models[name] = model
+
             setattr(module, name, model)
     # second pass -- update type ascriptions to point to real objects now that
     # they're all realized
